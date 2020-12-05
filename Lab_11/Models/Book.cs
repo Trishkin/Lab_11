712f5226b0c9f5b3b9ff1994684a522684dc7d0b
elik <elik@example.com>
2020-12-05 23:18:54 +0300

    first commit

---
+++ Lab_11/Models/Book.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace Lab_11.Models
+{
+    class Book
+    {
+        public static int count = 0;
+        private readonly int id = 1;
+        private string namebook = "Why do I live";
+        public string Namebook
+        {
+            get { return namebook; }
+            private set { namebook = value; }
+        }
+        private string author { get; set; } = "Dimasik";
+        public string Author
+        {
+            get { return author; }
+            set { author = value; }
+        }
+        private const string devel = "BSTU";
+        public string Devel
+        {
+            get { return devel; }
+            set { }
+        }
+        private int age = 2020;
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+        private int page = 456;
+        public int Page
+        {
+            get { return page; }
+            set { page = value; }
+        }
+        private int price = 105;
+        public int Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+        private string typeofcover = "soft";
+        public string Typeofcover
+        {
+            get { return typeofcover; }
+            set { typeofcover = value; }
+        }
+
+        public Book(int id, string Namebook, string author, int age, int page, int price, string typeofcover)
+        {
+            this.id = id;
+            this.Namebook = Namebook;
+            this.author = author;
+            this.age = age;
+            this.page = page;
+            this.price = price;
+            this.typeofcover = typeofcover;
+            count++;
+        }
+
+        public Book()
+        {
+            id = 4534568;
+            Namebook = "How to become more skillfull in MTG";
+            author = "Dmritriy Kulikov";
+            age = 2019;
+            page = 342;
+            price = 20;
+            typeofcover = "hard";
+            count++;
+        }
+
+        public Book(int joga)
+        {
+            id = joga;
+            count++;
+        }
+
+        public void Info()
+        {
+            Console.WriteLine($"id: {id}\nNamebook: {Namebook}\nauthor: {author}\ndevel: {devel}\nage: {age}\npage: {page}\nprice: {price}\ntypeofcover: {typeofcover}");
+        }
+
+        public static void InfoClass()
+        {
+            Console.WriteLine($"This class have fields: id, author, devel, age, page, price, typeofcover");
+        }
+
+        public void sum(ref int a, ref int b, out int c)
+        {
+            c = a + b;
+        }
+
+
+        private Book(int yoi, int oiy)
+        {
+            page = yoi;
+            price = oiy;
+        }
+    }
+}
---
+++ Lab_11/Program.cs
@@ -0,0 +1,121 @@
+﻿using Lab_11.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            part1();
+            part2();
+        }
+
+        static void part1()
+        {
+            string[] month = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+            string[] SumNWin = { "January", "February", "June", "July", "August", "December" };
+            //Console.WriteLine($"Введите n (длина строки)");
+            //int lenght = Convert.ToInt32(Console.ReadLine());
+            var selectedMonth = month.Where(t => t.Length == 7).OrderBy(t => t);
+            foreach (string s in selectedMonth)
+                Console.WriteLine(s);
+
+            Console.WriteLine();
+
+            selectedMonth = month.Where(t => SumNWin.Contains(t)).OrderBy(t => t);
+            foreach (string s in selectedMonth)
+                Console.WriteLine(s);
+
+            Console.WriteLine();
+
+            selectedMonth = month.OrderBy(t => t);
+            foreach (string s in selectedMonth)
+                Console.WriteLine(s);
+
+            Console.WriteLine();
+
+            selectedMonth = month.Where(t => t.IndexOf("u") != -1 && t.Length >= 4).OrderBy(t => t);
+            foreach (string s in selectedMonth)
+                Console.WriteLine(s);
+
+            Console.WriteLine();
+        }
+        static void part2()
+        {
+            List<Book> ListOfBooks = new List<Book>();
+            ListOfBooks.Add(new Book(1, "LiveToWin", "Angel", 2001, 100, 2100, "soft"));
+            ListOfBooks.Add(new Book(2, "LiveWin", "Agel", 2007, 150, 1100, "soft"));
+            Book book3 = new Book(3, "ToWin", "gel", 2004, 170, 5100, "soft");
+            ListOfBooks.Add(book3);
+            ListOfBooks.Add(new Book(4, "Live", "Anel", 2005, 110, 2152, "soft"));
+            ListOfBooks.Add(new Book(5, "Win", "Cris", 2000, 102, 2750, "soft"));
+            ListOfBooks.Add(new Book(6, "LiveTo", "Briz", 1989, 104, 2999, "soft"));
+            ListOfBooks.Add(new Book(7, "To", "Jovany", 1955, 116, 2147, "soft"));
+
+            var selectedBook = ListOfBooks.Where(t => t.Author == "Agel" && t.Age == 2007).OrderBy(t => t);
+            foreach (Book s in selectedBook)
+            {
+                s.Info();
+                Console.WriteLine();
+            }
+            Console.WriteLine("__________________________________________________________");
+
+            var selectedAge = ListOfBooks.Where(t => t.Age > 2001);
+            foreach (Book s in selectedAge)
+            {
+                s.Info();
+                Console.WriteLine();
+            }
+            Console.WriteLine("__________________________________________________________");
+
+            var PageMin = ListOfBooks.Where(t => t.Page == ListOfBooks.Min(n => n.Page));
+            foreach (Book s in PageMin)
+            {
+                s.Info();
+                Console.WriteLine();
+            }
+            Console.WriteLine("__________________________________________________________");
+
+            var LowPrice = ListOfBooks.OrderByDescending(t => t.Page).Take(5).OrderBy(t => t.Price);
+            foreach (Book s in LowPrice)
+            {
+                s.Info();
+                Console.WriteLine();
+            }
+            Console.WriteLine("__________________________________________________________");
+
+            var Sort = ListOfBooks.OrderBy(t => t.Price);
+            foreach (Book s in Sort)
+            {
+                s.Info();
+                Console.WriteLine();
+            }
+            Console.WriteLine("__________________________________________________________");
+
+
+
+            Console.WriteLine("Task4");
+            var My = ListOfBooks.Where(t => t.Age > 2001 && ListOfBooks.Contains(book3)).OrderByDescending(t => t.Page).Take(3).First();
+                My.Info();
+                Console.WriteLine();
+            Console.WriteLine("__________________________________________________________");
+
+
+            List<Book> ListOfBooks1 = new List<Book>();
+            ListOfBooks1.Add(new Book(1, "LiveToWin", "Angel", 2001, 100, 2100, "soft"));
+            ListOfBooks1.Add(new Book(2, "LiveWin", "Agel", 2007, 150, 1100, "soft"));
+            var result = from t in ListOfBooks 
+                         join n in ListOfBooks1 on t.Namebook equals n.Namebook 
+                         select  new Book(1, t.Namebook, n.Author, 2001, 100, 2100, "soft") ;
+            foreach (Book s in result)
+            {
+                s.Info();
+                Console.WriteLine();
+            }
+        }
+       
+    }
+}
